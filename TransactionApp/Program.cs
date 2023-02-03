dfc219875b29d2de1ce2a59e479fa3e209084be4
usonkubatbekov <123345450+usonkubatbekov@users.noreply.github.com>
2023-02-03 16:49:54 +0600

    asdasd

--- TransactionApp/Program.cs
+++ TransactionApp/Program.cs
@@ -12,7 +12,7 @@ using System.IO;
 using System.Text.Json;
 using System.Timers;
 using System.Threading;
-
+using System.Globalization;
 
 namespace TransactionApp
 {
@@ -31,64 +31,12 @@ namespace TransactionApp
                 {
                     case "add":
                         {
-                            Transaction transaction = new Transaction();
-                            Console.WriteLine("Введите ID:");
-                            transaction.Id = int.Parse(Console.ReadLine());
-                            try
-                            {
-                                Console.WriteLine("Введите дату(День.месяц.Год):");
-                                transaction.TransactionDate = DateTime.Parse(Console.ReadLine());
-                            }
-                            catch
-                            {
-                                Console.WriteLine("Некореткно введена дата");
-                                break;
-                            }
-
-                            try
-                            {
-                                Console.WriteLine("Введите cумму:");
-                                transaction.Amount = decimal.Parse(Console.ReadLine());
-
-                            }
-                            catch
-                            {
-                                Console.WriteLine("Некореткно введена сумма");
-                            }
-
-                            using (FileStream fs = new FileStream("transaction.json", FileMode.OpenOrCreate))
-                                fs.Close();
-                            string path = Directory.GetCurrentDirectory();
-                            string subputh = @"\transaction.json";
-                            var filename = string.Concat(path,subputh);
-                            var jsondata = System.IO.File.ReadAllText(filename);
-                            List<Transaction> existData = JsonConvert.DeserializeObject<List<Transaction>>(jsondata) ?? new List<Transaction>();
-                            existData.Add(transaction);
-                            jsondata = JsonConvert.SerializeObject(existData);
-                            File.WriteAllText(filename, jsondata);
-                            Console.WriteLine("Данные сохранены!");
+                            WriteFile();
                             break;
                         }
                     case "get":
                         {
-                            Console.WriteLine("Введите ID");
-                            int ID = Convert.ToInt32(Console.ReadLine());
-                            using (FileStream fs = new FileStream("transaction.json", FileMode.OpenOrCreate))
-                                fs.Close();
-                            string path = Directory.GetCurrentDirectory();
-                            string subputh = @"\transaction.json";
-                            var filename = string.Concat(path, subputh);
-                            var jsondata = System.IO.File.ReadAllText(filename);
-                            
-                            List<Transaction> existData = JsonConvert.DeserializeObject<List<Transaction>>(jsondata);
-                            foreach ( Transaction transaction in existData ) 
-                            {
-                                if (ID == transaction.Id)
-                                {
-                                    var Jsondata = JsonConvert.SerializeObject(transaction);
-                                    Console.WriteLine(Jsondata);
-                                }
-                            }
+                            GetFile();
                             break;
                            
                         }
@@ -108,6 +56,61 @@ namespace TransactionApp
                 Console.ReadLine();
             }
         
+        }
+        private static void WriteFile()
+        {
+            Transaction transaction = new Transaction();
+            Console.WriteLine("Введите ID:");
+            transaction.Id = int.Parse(Console.ReadLine());
+            try
+            {
+                NumberFormatInfo number = new NumberFormatInfo() 
+                {
+                    NumberDecimalSeparator= ".",
+                };
+                Console.WriteLine("Введите дату(День.месяц.Год):");
+                transaction.TransactionDate = DateTime.Parse(Console.ReadLine(),number);
+                Console.WriteLine("Введите cумму:");
+                transaction.Amount = decimal.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Некореткно введены данные");
+            }
+            var item = transaction;
+            using (FileStream fs = new FileStream("transaction.json", FileMode.OpenOrCreate))
+                fs.Close();
+            string path = Directory.GetCurrentDirectory();
+            string subputh = @"\transaction.json";
+            var filename = string.Concat(path, subputh);
+            var jsondata = System.IO.File.ReadAllText(filename);
+            List<Transaction> existData = JsonConvert.DeserializeObject<List<Transaction>>(jsondata) ?? new List<Transaction>();
+            existData.Add(transaction);
+            jsondata = JsonConvert.SerializeObject(existData);
+            File.WriteAllText(filename, jsondata);
+            Console.WriteLine("Данные сохранены!");
+        }
+        private static void GetFile()
+        {
+            Console.WriteLine("Введите ID");
+            int ID = Convert.ToInt32(Console.ReadLine());
+            using (FileStream fs = new FileStream("transaction.json", FileMode.OpenOrCreate))
+                fs.Close();
+            string path = Directory.GetCurrentDirectory();
+            string subputh = @"\transaction.json";
+            var filename = string.Concat(path, subputh);
+            var jsondata = System.IO.File.ReadAllText(filename);
+
+            List<Transaction> existData = JsonConvert.DeserializeObject<List<Transaction>>(jsondata);
+            foreach (Transaction transaction in existData)
+            {
+                if (ID == transaction.Id)
+                {
+                    var Jsondata = JsonConvert.SerializeObject(transaction);
+                    Console.WriteLine(Jsondata);
+                }
+            }
+
         }
     }
 }
